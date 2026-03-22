ce1b68856c4a482f93e86f9b772db02a948b7660
aminmguidich <medamine.mguidich@gmail.com>
2026-03-22 20:07:37 +0100

    feat: add user management components and styles
    - Implemented a header component with styling and logo.
    - Created a user component to display user information with avatars.
    - Added dummy user data for testing purposes.
    - Updated app component to include header and user components.
    - Enhanced global styles and index.html for better layout and design.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { User } from "./user/user";
+import { DUMMY_USERS } from './dummy-users';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, User],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular-project');
-}
+  users = DUMMY_USERS;
+} 
---
+++ src/app/user/user.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.html',
+  styleUrl: './user.css',
+})
+export class User {
+  @Input() avatar: string = '';
+  @Input() name: string = '';
+
+  get imagePath(): string {
+    return `assets/users/${this.avatar}`;
+  }
+
+  OnSelectUser() {}
+}
