1724476556ed685fbd2918d8e634a3c3a8670e06
aminmguidich <medamine.mguidich@gmail.com>
2026-03-23 17:02:29 +0100

    feat: enhance task component with dynamic data and user input

--- src/app/tasks/task/task.ts
+++ src/app/tasks/task/task.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
+interface TaskModel {
+  id: string;
+  userId: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+}
 @Component({
   selector: 'app-task',
   imports: [],
   templateUrl: './task.html',
   styleUrl: './task.css',
 })
-export class Task {}
+export class Task {
+  @Input({ required: true }) task!: TaskModel;
+}
